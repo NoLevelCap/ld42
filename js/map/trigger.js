88789331dc079d1fdd33a6b5f481c1d9080d60ed
SirBozwellox <jackgloyens@googlemail.com>
2018-08-12 00:14:42 +0100

    Added player textures

--- js/map/trigger.js
+++ js/map/trigger.js
@@ -1,23 +1,5 @@
 function triggerCode(){
   this.ghostTrigger = function(trigger){
-    trigger.objData.inactive = true;
-
-    var ghostSpawnTrigger;
-
-    for (var triggerid in GAMEMANAGER.Map.triggerdata[GAMEMANAGER.Map.currentFloor]) {
-      if (GAMEMANAGER.Map.triggerdata[GAMEMANAGER.Map.currentFloor].hasOwnProperty(triggerid)) {
-        if(GAMEMANAGER.Map.triggerdata[GAMEMANAGER.Map.currentFloor][triggerid].id == trigger.objData.spawnid){
-          ghostSpawnTrigger = GAMEMANAGER.Map.triggerdata[GAMEMANAGER.Map.currentFloor][triggerid];
-        }
-      }
-    }
-
-    newGhost = new ghost();
-    newGhost.pivot.set(newGhost.width/2, newGhost.height/2);
-    newGhost.position.set(ghostSpawnTrigger.x + ghostSpawnTrigger.width/2, ghostSpawnTrigger.y + ghostSpawnTrigger.height/2);
-    GAMEMANAGER.Map.ghostLayer.addChild(newGhost);
+      console.log("Trigger");
   };
-  this.ghostSpawn = function(trigger){
-    
-  }
 }
