b3fdaa97b21d5514fcdaffc23f5379e2f32d62cc
SirBozwellox <jackgloyens@googlemail.com>
2018-08-12 15:40:30 +0100

    Added loads of cool shit

--- js/player.js
+++ js/player.js
@@ -6,6 +6,12 @@ function player() {
   _this.facing;
   _this.inventory = new Array();
   _this.spriteList = new Array();
+  _this.moveD = 0;
+  _this.moveS = 0;
+  _this.movementTimerReset = 10;
+  _this.movementTimer = 0;
+  _this.playerRotate;
+//  _this.qFlag = FALSE;
 
   this.init = function(){
     _this.spriteList.push(new Sprite(Tex_Main["player1_nw.png"]));
@@ -26,19 +32,56 @@ function player() {
     GAMEMANAGER.animatables.push(_this);
 
     document.addEventListener('keydown', _this.onKeyDown);
+    document.addEventListener('keyup', _this.onKeyUp);
   }
 
   this.onKeyDown = function(key){
+
+    if (key.keyCode === 81 || key.keyCode === 32) {
+      GAMEMANAGER.overlay.switch();
+    }
+
     // W Key is 87
     // Up arrow is 38
     if (key.keyCode === 87 || key.keyCode === 38) {
-      _this.move(1);
+      _this.moveD = 1;
     }
 
     // S Key is 83
     // Down arrow is 40
     if (key.keyCode === 83 || key.keyCode === 40) {
-      _this.move(-1);
+      _this.moveD = -1;
+    }
+
+    // A Key is 65
+    // Left arrow is 37
+    if (key.keyCode === 65 || key.keyCode === 37) {
+      _this.moveS = -1;
+    }
+
+    // D Key is 68
+    // Right arrow is 39
+    if (key.keyCode === 68 || key.keyCode === 39) {
+      _this.moveS = 1;
+    }
+
+    if (key.keyCode === 69) {
+      if (!paused) {
+        GAMEMANAGER.textmanager.showText();
+      } else {
+        GAMEMANAGER.textmanager.hideText();
+      }
+    }
+  }
+
+  this.onKeyUp = function(key) {
+    if (key.keyCode === 87 || key.keyCode === 38 || key.keyCode === 83 || key.keyCode === 40) {
+      _this.moveD = 0;
+      _this.movementTimer = 0;
+    }
+    if (key.keyCode === 65 || key.keyCode === 37 || key.keyCode === 68 || key.keyCode === 39) {
+      _this.moveS = 0;
+      _this.movementTimer = 0;
     }
   }
 
@@ -55,11 +98,23 @@ function player() {
   }
 
   this.animatable = function(){
+
+    if (!paused) {
+
+    if (_this.moveD != 0 || _this.moveS != 0) {
+      _this.movementTimer -= 1;
+      if (_this.movementTimer <= 0) {
+        _this.movementTimer = _this.movementTimerReset;
+        _this.move(_this.moveD, _this.moveS);
+      }
+    }
+
     var mousePos = renderer.plugins.interaction.mouse.global;
 
     var rot = Math.atan2(mousePos.y - renderer.height/2, mousePos.x - renderer.width/2);
 
-//    _this.rotation = rot + (90*DEG2RAD);
+    _this.playerRotate = rot + (90*DEG2RAD);
+//    _this.rotation = this.playerRotate;
 
     var rotTestValue = rot + Math.PI;
     if((Math.PI*(3/8)) >= rotTestValue && rotTestValue >= (Math.PI*(1/8))){
@@ -87,29 +142,45 @@ function player() {
       _this.setFacing("W");
       _this.changeSprite(7);
     }
-
+  }
   }
 
   this.setFacing = function(dir){
     _this.facing = dir;
   }
 
-  this.move = function(d){
+  this.move = function(d, s){
     var dx = 0, dy = 0;
-    if (_this.facing.includes('S')
-    && !(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy + d)) ) {
-      dy = d;
-    } else if (_this.facing.includes('N')
-    && !(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy - d)) ) {
-      dy = -d;
+    if (_this.facing.includes('S')) {
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy + d)) && d != 0) {
+        dy = d;
+      }
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx - s, _this.mapy)) && s != 0) {
+        dx = -s;
+      }
+    } else if (_this.facing.includes('N')) {
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy - d)) && d != 0) {
+        dy = -d;
+      }
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx + s, _this.mapy)) && s != 0) {
+        dx = s;
+      }
     }
 
-    if (_this.facing.includes('E')
-    && !(GAMEMANAGER.Map.checkCollision(_this.mapx + d, _this.mapy)) ) {
-      dx = d;
-    } else if (_this.facing.includes('W')
-    && !(GAMEMANAGER.Map.checkCollision(_this.mapx - d, _this.mapy)) ) {
-      dx = -d;
+    if (_this.facing.includes('E')) {
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx + d, _this.mapy)) && d != 0) {
+        dx = d;
+      }
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy + s)) && s != 0) {
+        dy = s;
+      }
+    } else if (_this.facing.includes('W')) {
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx - d, _this.mapy)) && d != 0) {
+        dx = -d;
+      }
+      if (!(GAMEMANAGER.Map.checkCollision(_this.mapx, _this.mapy - s)) && s != 0) {
+        dy = -s;
+      }
     }
 
     if(!(GAMEMANAGER.Map.checkCollision(_this.mapx + dx, _this.mapy + dy))){
